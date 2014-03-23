bdd77d49b44670abf28ab1cf327fb31d530b6593
Paul Draper <paulddraper@gmail.com>
2014-03-22 22:48:07 -0600

    Add Cache-Control to HTML pages

--- app/arch/controllers/ArchController.scala
+++ app/arch/controllers/ArchController.scala
@@ -9,13 +9,13 @@ import play.api.Play.{current => application}
 object ArchController extends Controller {
 
   def index() = Action {
-    Ok(views.html.map())
+    Ok(views.html.map()).withHeaders(CACHE_CONTROL -> "public, max-age=3600")
   }
 
   def building(key: String) = Action {
     application.resourceAsStream(s"arch/shapes/data/$key.json") map { stream =>
       val buildingJson = Json.parse(IOUtils.toByteArray(stream))
-      Ok(views.html.building(buildingJson))
+      Ok(views.html.building(buildingJson)).withHeaders(CACHE_CONTROL -> "public, max-age=3600")
     } getOrElse {
       NotFound("Not found")
     }
